0ba8086c95d0c37513864a98d5ff04ec0a3f45fb
Abhi0704 <abhijeetsingh0704@gmail.com>
2020-05-07 16:26:47 +0530

    first commit

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,5 +1,7 @@
+num1 = input(range())
+
 def fizzbuzz():
-    for num in range(1,101):
+    for num in num1():
         if num%3 == 0 and num%5 == 0:
             print("fizzbuzz")
         elif num%3 == 0:
--- mycalc_fixed.py
+++ mycalc_fixed.py
@@ -6,9 +6,12 @@ def subtract(x,y):
 
 def multiply(x,y):
     return x * y
-
+#try catching the zzerodivisionerror by try and except method here
 def divide(x,y):
-    return x / y
+    try:
+        return x / y
+    except ZeroDivisionError:
+        print("0, Anything you divide by 0 is always 0")
 print("select 1 = add, 2 = subtraction, 3= multiplication, 4= divison")
 choice = input("Enter your choice (1/2/3/4): ")
 num1 = float(input("please enter your first number : "))
